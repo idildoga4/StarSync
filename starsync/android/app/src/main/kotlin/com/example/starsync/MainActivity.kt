18c90b43f3cb73a7c0b220adcbc76e158d98ff7c
idildoga4 <111755801+idildoga4@users.noreply.github.com>
2023-11-29 22:50:07 +0300

    loginpage

---
+++ starsync/android/app/src/main/kotlin/com/example/starsync/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.starsync
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ starsync/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:starsync/screens/auth.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Color.fromARGB(255, 231, 185, 221)),
+      ),
+      home: const AuthScreen(),
+    );
+  }
+}
