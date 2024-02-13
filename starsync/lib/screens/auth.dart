62333628bbe2e490152c63c25c3710d0b471440d
İdil Doğan <idildoga4@hotmail.com>
2024-02-13 03:23:47 +0300

    firebadd

--- starsync/lib/screens/auth.dart
+++ starsync/lib/screens/auth.dart
@@ -24,8 +24,10 @@ class _AuthScreenState extends State<AuthScreen>
   var _isLogin=true;
   var _enteredEmail='';
   var _enteredPassword= '';
+  var _enteredUsername='';
   File? _selectedImage;
   var _isAuthenticating=false;
+  
 
   void _submit() async {
     final isValid=_formKey.currentState!.validate();
@@ -56,7 +58,7 @@ class _AuthScreenState extends State<AuthScreen>
 
   await FirebaseFirestore.instance.collection('users').doc(UserCredentials.user!.uid).set(
     {
-    'username':'to be done',
+    'username':_enteredUsername,
     'email': _enteredEmail,
     'image_url':imageUrl,
     }
@@ -135,7 +137,23 @@ class _AuthScreenState extends State<AuthScreen>
                             _enteredEmail=value!;
                           } ,
                         ),
-                        
+                        if(!_isLogin)
+                        TextFormField(
+                          decoration:InputDecoration(labelText:'Username'),
+                          enableSuggestions:false,
+                          validator:(value)
+                          {
+                            if(value==null||value.isEmpty||value.trim().length<4)
+                            {
+                              return 'Please enter at least 4 characters.';
+                            }
+                            return null;
+                          }
+                          onSaved:(value)
+                          {
+                            _enteredUsername=value!;
+                          }
+                        )
                         TextFormField(
                           decoration: const InputDecoration(
                             labelText:'Password'
